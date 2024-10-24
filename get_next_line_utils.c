52cb07f1e7e04dd447e54af1dba2ad8fd9ae700a
Samvel <115891697+assam4@users.noreply.github.com>
2024-10-25 02:06:54 +0400

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -27,31 +27,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 	return (src_len);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*ptr;
-	size_t	s1_len;
-	size_t	s2_len;
-
-	if (s1 && s2)
-	{
-		s1_len = ft_strlen(s1);
-		s2_len = ft_strlen(s2);
-		ptr = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
-		if (ptr)
-		{
-			ft_strlcpy(ptr, s1, s1_len + 1);
-			ft_strlcpy(ptr + s1_len, s2, s2_len + 1);
-		}
-		return (ptr);
-	}
-	else if (!s1)
-		return (ft_strdup(s2));
-	else if (!s2)
-		return (ft_strdup(s1));
-	return (NULL);
-}
-
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*iter;
@@ -81,5 +56,28 @@ char	*ft_strdup(const char *s)
 	ptr = (char *)malloc((s_len) * sizeof(char));
 	if (ptr)
 		ft_strlcpy(ptr, s, s_len);
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ptr = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (ptr)
+	{
+		ft_strlcpy(ptr, s1, s1_len + 1);
+		ft_strlcpy(ptr + s1_len, s2, s2_len + 1);
+	}
 	return (ptr);
 }
