e85f4fc8e8aa0e60e4378948461985a2cc8cd6d1
assam4 <samaslanyan3@gmail.com>
2025-01-19 22:28:58 +0400

    after tests

--- get_next_line.c
+++ get_next_line.c
@@ -48,7 +48,7 @@ static int	push(int fd, char **buffer, size_t *capacity, size_t *readed)
 	}
 	return (read_bit);
 }
-
+/*
 static size_t	length(char **endline, char **buffer, size_t readed)
 {
 	size_t	len;
@@ -86,29 +86,74 @@ static char	*pop(char **buffer, size_t *readed)
 	}
 	return (outline);
 }
+*/
+
+static char	*pop(char **buffer, size_t *readed)
+{
+	char	*outline;
+	char	*endline;
+	size_t	linelen;
+
+	if (!*buffer || !**buffer)
+		return (NULL);
+	endline = ft_strchr(*buffer, NEWLINE);
+	if (!endline)
+		linelen = ft_strlen(*buffer);
+	else if ((size_t)(endline - *buffer + 1) > *readed)
+		linelen = *readed;
+	else
+		linelen = endline - *buffer + 1;
+	outline = (char *)ft_calloc(linelen + 1, sizeof(char));
+	if (outline)
+	{
+		ft_strlcpy(outline, *buffer, linelen + 1);
+		*readed -= linelen;
+		if (endline)
+			ft_strlcpy(*buffer, endline + 1, *readed + 1);
+		else
+			ft_memset(*buffer, 0, linelen);
+	}
+	return (outline);
+}
+
+void	arg_free(void **buffer, void **capacity, void **readed_count)
+{
+	if (*buffer)
+	{
+		free(*buffer);
+		*buffer = NULL;
+	}
+	if (*capacity)
+	{
+		free(*capacity);
+		*capacity = NULL;
+	}
+	if (*readed_count)
+	{
+		free(*readed_count);
+		*readed_count = NULL;
+	}
+}
 
 char	*get_next_line(int fd)
 {
-	static char		*buffer = NULL;
-	static size_t	capacity = 0;
-	static size_t	readed_count = 0;
+	static void		*data[3] = {NULL};
 	char			*outline;
 	int				read_bit;
 
-	if (fd != 0 && fd < 2)
+	if ((fd != 0 && fd < 2) || fd > 65535) // 2^16
 		return (NULL);
-	read_bit = push(fd, &buffer, &capacity, &readed_count);
+	if (!data[1])
+		data[1] = ft_calloc(1, sizeof(size_t));
+	if (!data[2])
+		data[2] = ft_calloc(1, sizeof(size_t));
+	read_bit = push(fd, (char **)&data[0],
+			(size_t *)data[1], (size_t *)data[2]);
 	if (read_bit >= 0)
-		outline = pop(&buffer, &readed_count);
-	if (read_bit < 0 || !readed_count)
+		outline = pop((char **)&data[0], (size_t *)data[2]);
+	if (read_bit < 0 || !*((size_t *)data[2]))
 	{
-		if (buffer)
-		{
-			free(buffer);
-			buffer = NULL;
-		}
-		readed_count = 0;
-		capacity = 0;
+		arg_free(&data[0], &data[1], &data[2]);
 		if (read_bit < 0)
 			return (NULL);
 	}
